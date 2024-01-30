5d906d7dde24b04c9342f97940c561632f89811d
Mukund Raj <mukundraj@Mukunds-MacBook-Air.local>
2024-01-31 01:04:18 +0530

    swagger log and add swagger UI

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ require("./db");
 //app routing
 const fs = require("fs");
 
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+  })
+);
+
 app.use(
   helmet({
     contentSecurityPolicy: {
